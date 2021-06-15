6ac938918122b6f267d538414b4f82153c3595ac
jakeFromm <jacobfromm@gmail.com>
2021-06-15 18:39:49 -0400

    main image not displaying in local server

--- components/MainImage.js
+++ components/MainImage.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import Image from 'next/image'
 
 export default function MainImage() {
     return(
         <div className="container mx-auto px-6">
-      <h1 className="text-white text-2xl font-medium">Main Image</h1>
-      </div>
+            <h1 className="text-white text-2xl font-medium">Main Image</h1>
+            <Image 
+                src="/images/logo.png"
+                alt="picture of the InQueery Media campus"
+                width={5760}
+                height={3600}
+                layout='responsive'
+            />
+        </div>
     )
 }
